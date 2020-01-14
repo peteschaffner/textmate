05db574543a7aa6c3af8d9e982354e2d1f80b9fe
Pete Schaffner <pschaffner@google.com>
2020-01-14 17:02:30 +0100

    Tweak file browser action icons scale and spacing

--- Frameworks/FileBrowser/src/OFB/OFBActionsView.mm
+++ Frameworks/FileBrowser/src/OFB/OFBActionsView.mm
@@ -62,7 +62,7 @@ static NSButton* OakCreateImageButton (NSImage* image)
 		OakAddAutoLayoutViewsToSuperview([views allValues], self);
 		OakSetupKeyViewLoop(@[ self, _createButton, _actionsPopUpButton, _reloadButton, _searchButton, _favoritesButton, _scmButton ], NO);
 
-		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-8-[create]-[divider]-[actions(==31)]-(>=8)-[reload]-8-[search]-8-[favorites]-8-[scm]-(12)-|" options:NSLayoutFormatAlignAllCenterY metrics:nil views:views]];
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-8-[create]-[divider]-[actions(==31)]-(>=8)-[reload]-8-[search]-8-[favorites]-8-[scm]-(9)-|" options:NSLayoutFormatAlignAllCenterY metrics:nil views:views]];
 		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[divider(==reload)]|"                                                                               options:0 metrics:nil views:views]];
 	}
 	return self;
